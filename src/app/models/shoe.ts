b8fca97ba73cb77b34b8a2d424eb8ff98744c9e2
Pierre Henri <payhache@gmail.com>
2020-06-27 14:23:18 +0200

    creation class Shoe

---
+++ src/app/models/shoe.ts
@@ -0,0 +1,15 @@
+export class Shoe {
+    id:number;
+    nom:string;
+    marque:string;
+    type:string;
+    taille:number;
+    dateEntreStock:Date;
+    constructor(id:number=null,nom:string=null,marque:string=null,type:string=null,taille:number=null,dateEntreStock:Date=null ){
+        this.id = id;
+        this.nom = nom;
+        this.marque = marque;
+        this.taille = taille;
+        this.dateEntreStock = dateEntreStock;
+    }
+}
